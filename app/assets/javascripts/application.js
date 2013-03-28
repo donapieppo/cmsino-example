03000e21c9f30398aabb24dfbe690b70f7c62fa6
donapieppo <donapieppo@yahoo.it>
2013-03-28 11:20:25 +0100

    using wysihtml5

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,5 +13,6 @@
 //= require jquery
 //= require jquery_ujs
 //= require twitter/bootstrap
-//= require nicedit/nicEdit.js
+//= require wysihtml5/wysihtml5-0.3.0.min.js
+//= require wysihtml5/bootstrap-wysihtml5.js
 //= require_tree .
