02b39327a7c97cde607e16a461e440699e62b36b
Donapieppo <donapieppo@yahoo.it>
2014-02-18 17:35:42 +0100

    moved wysihtml5 and bootstrap in cmsino layout

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,7 +13,5 @@
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
-//= require twitter/bootstrap
-//= require wysihtml5/wysihtml5-0.3.0.min.js
-//= require wysihtml5/bootstrap-wysihtml5.js
+//= require cmsino
 //= require_tree .
