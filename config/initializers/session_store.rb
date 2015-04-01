92b46460ae4e88007db780d2878887a10c810a20
Donapieppo <donapieppo@yahoo.it>
2015-04-01 15:56:43 +0200

    rails 4.2

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-CmsinoExample::Application.config.session_store :cookie_store, key: '_cmsino-example_session'
+Rails.application.config.session_store :cookie_store, key: '_cmsino-example_session'
