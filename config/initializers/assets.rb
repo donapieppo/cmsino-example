0e117c4b65dffb5664f6ad41d0b5f02b62b979c4
Pietro Donatini <pietro.donatini@unibo.it>
2015-04-07 16:39:02 +0200

    FIXME

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -9,3 +9,4 @@ Rails.application.config.assets.version = '1.0'
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
+Rails.application.config.assets.precompile += %w( cmsino/cmsino_internal.css cmsino/cmsino_internal.js )
