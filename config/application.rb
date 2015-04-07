b254d7efc39e2fd46133f0461b5e63e193fd2cb9
Pietro Donatini <pietro.donatini@unibo.it>
2015-04-07 16:24:36 +0200

    strange...

--- config/application.rb
+++ config/application.rb
@@ -19,5 +19,7 @@ module CmsinoExample
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+    I18n.available_locales = [:it, :en, :es]
+    config.i18n.default_locale = :it
   end
 end
