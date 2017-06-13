5174ab4a595a3832b99a3eecfe00e1c806e79ef0
Pietro <donapieppo@yahoo.it>
2017-06-13 16:23:41 +0200

    jquery

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,8 +10,8 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require rails-ujs
-//= require bootstrap-sprockets
 //= require jquery
+//= require jquery_ujs
+//= require bootstrap-sprockets
 //= require cmsino
 //= require_tree .
