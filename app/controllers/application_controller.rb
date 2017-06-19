ea640ae3c445c8b6af76491904a327af748b7d6a
Pietro <donapieppo@yahoo.it>
2017-06-19 15:50:59 +0200

    locale in session

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,16 +3,15 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  include CmsinoHelper
+  include Cmsino::ControllerHelpers
 
   before_action :set_locale
 
   def set_locale
-    locale = 'it'
     if params[:locale] and Cmsino::Conf.instance.locales.include?(params[:locale].to_s)
-     locale = params[:locale]
+      session[:locale] = params[:locale]
     end
 
-    I18n.locale = locale || I18n.default_locale
+    I18n.locale = session[:locale] || I18n.default_locale || 'it'
   end
 end
