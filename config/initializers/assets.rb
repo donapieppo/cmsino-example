242c2bbe7f9a1a8b775a18305d6ca93704d17d7f
Pietro <donapieppo@yahoo.it>
2017-07-20 15:35:15 +0200

    following cmsino development

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -12,5 +12,5 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
 # Rails.application.config.assets.precompile += %w( admin.js admin.css )
-Rails.application.config.assets.precompile += %w( cmsino/cmsino_internal.css cmsino/cmsino_internal.js )
-Rails.application.config.assets.precompile += %w( nicEditorIcons.gif )
+#Rails.application.config.assets.precompile += %w( cmsino/cmsino_internal.css cmsino/cmsino_internal.js )
+#Rails.application.config.assets.precompile += %w( nicEditorIcons.gif )
